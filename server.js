31f2a5b593ed2e5c6b50ffe54c23cc5a1db2e1b8
Guillaume <guillaume.ferron@gmail.com>
2020-05-25 18:10:17 +0200

    init simple verify server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.text({
+  type: function(req) {
+    return 'text';
+  }
+}));
+
+app.post('/slack/events', function (req, res) {
+    payload = JSON.parse(req.body)
+    console.log(payload.challenge);
+    res.send(payload.challenge);
+});
+
+http.createServer(app).listen(3000);
